f451b8c276b042a5708011372eddacce80da4532
SJYssr <sjy18836196959@gmail.com>
2024-12-30 14:16:16 +0800

    完成AI部分

--- demo1.py
+++ demo1.py
@@ -2,8 +2,25 @@ import tkinter as tk
 from ctypes import windll, wintypes
 
 from pynput.keyboard import Controller
+
+import _thread as thread
 import time
-# 定义SetWindowDisplayAffinity函数
+import base64
+import datetime
+import hashlib
+import hmac
+import json
+from urllib.parse import urlparse
+import ssl
+from datetime import datetime
+from time import mktime
+from urllib.parse import urlencode
+from wsgiref.handlers import format_date_time
+
+import websocket
+
+
+# 定义 SetWindowDisplayAffinity 函数
 SetWindowDisplayAffinity = windll.user32.SetWindowDisplayAffinity
 SetWindowDisplayAffinity.argtypes = [wintypes.HWND, wintypes.DWORD]
 SetWindowDisplayAffinity.restype = wintypes.BOOL
@@ -113,17 +130,143 @@ def switch_to_main():
     main_frame.pack(fill="both", expand=True)  # 显示主界面框架
 
 
-def AI_ask():
+def AI_ask(appid="1b69309b",
+         api_secret="YWY0MWJhNTM4MGU3NTJkZDJiMDM0ZjZl",
+         api_key="5b2302f3ac2295e56bd13f587d7ffa6e",
+        # appid、api_secret、api_key三个服务认证信息请前往开放平台控制台查看（https://console.xfyun.cn/services/bm35）
+         Spark_url= "wss://spark-api.xf-yun.com/v1.1/chat",
+        # Spark_url="wss://spark-api.xf-yun.com/v3.5/chat",   # Max环境的地址
+		# Spark_url = "wss://spark-api.xf-yun.com/v4.0/chat"  # 4.0Ultra环境的地址
+        # Spark_url = "wss://spark-api.xf-yun.com/v3.1/chat"  # Pro环境的地址
+        # Spark_url = "wss://spark-api.xf-yun.com/v1.1/chat"  # Lite环境的地址
+         domain= "lite"
+        # domain="generalv3.5",   # Max版本
+		# domain = "4.0Ultra"     # 4.0Ultra 版本
+        # domain = "generalv3"    # Pro版本
+        # domain = "lite"         # Lite版本址
+           ):
+    """AI搜索调用的函数"""
     ai_text_box.config(state='normal')  # 允许编辑 ai_text_box
     if ai_text_box.get('1.0', 'end-1c'):  # 检查 ai_text_box 是否有内容
         ai_text_box.delete('1.0', 'end')  # 清空AI回答文本框
-    """AI搜索调用的函数"""
-    # 此处填写调用ai的主函数
-    ai_text_box.insert('1.0', "ai功能未完善")
+    wsParam = Ws_Param(appid, api_key, api_secret, Spark_url)
+    websocket.enableTrace(False)
+    wsUrl = wsParam.create_url()
+    query = ai_search_entry.get()
+    ws = websocket.WebSocketApp(wsUrl, on_message=on_message, on_error=on_error, on_close=on_close, on_open=on_open)
+    ws.appid = appid
+    ws.query = query
+    ws.domain = domain
+    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
 
-    # 并且在ai回答的返回语句加入下一行代码实现显示
-    # ai_text_box.insert(tk.END, 此处填写具体的回答变量)  # 将AI回答插入到AI回答文本框中
-    # ai_text_box.config(state=tk.DISABLED)  # 将文本框设置为不可编辑状态
+
+class Ws_Param(object):
+    # 初始化
+    def __init__(self, APPID, APIKey, APISecret, gpt_url):
+        self.APPID = APPID
+        self.APIKey = APIKey
+        self.APISecret = APISecret
+        self.host = urlparse(gpt_url).netloc
+        self.path = urlparse(gpt_url).path
+        self.gpt_url = gpt_url
+
+    # 生成url
+    def create_url(self):
+        # 生成RFC1123格式的时间戳
+        now = datetime.now()
+        date = format_date_time(mktime(now.timetuple()))
+
+        # 拼接字符串
+        signature_origin = "host: " + self.host + "\n"
+        signature_origin += "date: " + date + "\n"
+        signature_origin += "GET " + self.path + " HTTP/1.1"
+
+        # 进行hmac-sha256进行加密
+        signature_sha = hmac.new(self.APISecret.encode('utf-8'), signature_origin.encode('utf-8'),
+                                 digestmod=hashlib.sha256).digest()
+
+        signature_sha_base64 = base64.b64encode(signature_sha).decode(encoding='utf-8')
+
+        authorization_origin = f'api_key="{self.APIKey}", algorithm="hmac-sha256", headers="host date request-line", signature="{signature_sha_base64}"'
+
+        authorization = base64.b64encode(authorization_origin.encode('utf-8')).decode(encoding='utf-8')
+
+        # 将请求的鉴权参数组合为字典
+        v = {
+            "authorization": authorization,
+            "date": date,
+            "host": self.host
+        }
+        # 拼接鉴权参数，生成url
+        url = self.gpt_url + '?' + urlencode(v)
+        # 此处打印出建立连接时候的url,参考本demo的时候可取消上方打印的注释，比对相同参数时生成的url与自己代码生成的url是否一致
+        return url
+
+
+# 收到websocket错误的处理
+def on_error(ws, error):
+    ai_text_box.insert('1.0', error)
+
+
+# 收到websocket关闭的处理
+def on_close(ws):
+    ai_text_box.insert('1.0', '##连接已关闭##')
+
+
+# 收到websocket连接建立的处理
+def on_open(ws):
+    thread.start_new_thread(run, (ws,))
+
+
+def run(ws, *args):
+    data = json.dumps(gen_params(appid=ws.appid, query=ws.query, domain=ws.domain))
+    ws.send(data)
+
+
+# 收到websocket消息的处理
+def on_message(ws, message):
+    data = json.loads(message)
+    code = data['header']['code']
+    if code != 0:
+        ai_text_box.insert('1.0', f'请求错误: {code}, {data}')
+        ws.close()
+    else:
+        choices = data["payload"]["choices"]
+        status = choices["status"]
+        content = choices["text"][0]["content"]
+        ai_text_box.insert(tk.END, content)
+        if status == 2:
+            ai_text_box.config(state=tk.DISABLED)  # 将文本框设置为不可编辑状态
+            ws.close()
+
+
+
+def gen_params(appid, query, domain):
+    """
+    通过appid和用户的提问来生成请参数
+    """
+
+    data = {
+        "header": {
+            "app_id": appid,
+            "uid": "1234",
+            # "patch_id": []    #接入微调模型，对应服务发布后的resourceid
+        },
+        "parameter": {
+            "chat": {
+                "domain": domain,
+                "temperature": 0.5,
+                "max_tokens": 4096,
+                "auditing": "default",
+            }
+        },
+        "payload": {
+            "message": {
+                "text": [{"role": "user", "content": query}]
+            }
+        }
+    }
+    return data
 
 root = tk.Tk()
 root.title("demo")
